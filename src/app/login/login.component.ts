e988efddac4dc0b644de3fe588f5ded1d1d975e6
Arj2001 <75932871+Arj2001@users.noreply.github.com>
2022-10-18 19:14:33 +0530

    login implimented

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private myRoute:Router) { }
+
+  username = ""
+  password = ""
+
+  readValue=()=>{
+    let data = {
+      "username":this.username,
+      "password":this.password,
+    }
+    if(this.username == "admin",this.password == "admin"){
+      this.myRoute.navigate(["/home"]) 
+      this.username= ""
+      this.password= ""
+    }else{
+      alert("Invalid credtionals")
+    }
+
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
